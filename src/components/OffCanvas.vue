edf0bb6525f5dc143ff9e0dc8db69036b36a282c
irobles1030 <irobles1030@gmail.com>
2024-09-03 22:43:42 +0000

    misc

--- src/components/OffCanvas.vue
+++ src/components/OffCanvas.vue
@@ -17,7 +17,7 @@
             target="_blank"
             class="block px-4 py-2 rounded-md transition-colors duration-200 hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-500"
           >
-            <h2 class="text-lg font-semibold">Shop On Etsy »</h2>
+            <h2 class="text-lg font-semibold text-center">Shop On Etsy »</h2>
           </a>
           
           <a 
