e241c94242c67a0770947960486d3d096e18722e
irobles1030 <irobles1030@gmail.com>
2024-09-05 20:12:28 +0000

    working loading bar

--- src/components/LoadingBar.vue
+++ src/components/LoadingBar.vue
@@ -1,51 +1,61 @@
 <template>
-    <div v-if="progress"
-      class="h-2 bg-blue-500 transition-all duration-300 ease-out z-[100]"
-      :style="{ width: `${progress}%` }"
-    ></div>
-  </template>
-  
-  <script setup>
-  import { ref, watch } from 'vue';
-  import { useRouter } from 'vue-router';
-  
-  const router = useRouter();
-  const progress = ref(0);
-  let timer = null;
-  
-  const startLoading = () => {
+  <div
+    v-if="isVisible"
+    class="fixed top-0 left-0 right-0 h-2 bg-blue-500 transition-all duration-300 ease-out z-[100]"
+    :style="{ width: `${progress}%` }"
+  ></div>
+</template>
+
+<script setup>
+import { ref, watch } from 'vue';
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+const progress = ref(0);
+const isVisible = ref(false);
+let timer = null;
+let hideTimer = null;
+
+const startLoading = () => {
+  clearTimeout(hideTimer);
+  isVisible.value = true;
+  progress.value = 0;
+  clearInterval(timer);
+  timer = setInterval(() => {
+    progress.value += (100 - progress.value) / 10;
+    if (progress.value > 95) {
+      clearInterval(timer);
+    }
+  }, 200);
+};
+
+const stopLoading = () => {
+  clearInterval(timer);
+  progress.value = 100;
+  hideTimer = setTimeout(() => {
+    isVisible.value = false;
     progress.value = 0;
-    timer = setInterval(() => {
-      progress.value += Math.random() * 10;
-      if (progress.value > 95) {
-        clearInterval(timer);
-      }
-    }, 1);
-  };
-  
-  const stopLoading = () => {
-    clearInterval(timer);
-    progress.value = 100;
-    setTimeout(() => {
-      progress.value = 0;
-    }, 300);
-  };
-  
-  watch(
-    () => router.currentRoute.value,
-    () => {
+  }, 500); // Delay hiding the bar
+};
+
+watch(
+  () => router.currentRoute.value,
+  (newRoute, oldRoute) => {
+    if (!isVisible.value && oldRoute.name) {
       startLoading();
     }
-  );
-  
-  router.beforeResolve((to, from, next) => {
-    if (to.path !== from.path) {
-      startLoading();
-    }
-    next();
-  });
-  
-  router.afterEach(() => {
-    stopLoading();
-  });
-  </script>
+  }
+);
+
+router.beforeEach((to, from, next) => {
+  if (to.path !== from.path) {
+    startLoading();
+  }
+  next();
+});
+
+router.afterEach(() => {
+  console.log('after each');
+  stopLoading();
+});
+</script>
