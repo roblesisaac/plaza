35011067d9360ffe16ebf60c619573cde87ef092
irobles1030 <irobles1030@gmail.com>
2024-09-06 00:45:40 +0000

    stripe test mode

--- api/services/stripeServices.js
+++ api/services/stripeServices.js
@@ -1,7 +1,7 @@
 import Stripe from 'stripe';
 import config from '../config/environment';
 
-const stripe = new Stripe(config.STRIPE.PRIVATE);
+const stripe = new Stripe(config.STRIPE.PRIVATE_TEST);
 
 export async function createCheckoutSession(email, lineItems) {
   const session = await stripe.checkout.sessions.create({
